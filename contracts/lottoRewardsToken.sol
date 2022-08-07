2e6da1ea13ad6e886dd7d2c6bffd99e381b7e00a
Chris <chriscrutt@gmail.com>
2022-08-07 13:59:58 -0700

    some renaming

--- contracts/lottoRewardsToken.sol
+++ contracts/lottoRewardsToken.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.15;
 
 import "./ERC80085.sol";
 
-contract LottoERC is  ERC80085 {
+contract LottoRewardsToken is  ERC80085 {
 
     // will be the lottery contract
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     
-    constructor() ERC20("Lotto Token", "LT") ERC20Permit("Lotto Token") {
+    constructor() ERC20("Lotto Rewards Token", "LT") ERC20Permit("Lotto Rewards Token") {
         _grantRole(DEFAULT_ADMIN_ROLE, tx.origin);
         _grantRole(MINTER_ROLE, _msgSender());
     }
