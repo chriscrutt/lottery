4ac1620a1ad016aa9d20496bb537a07fccb4db43
Chris <chriscrutt@gmail.com>
2022-12-05 15:10:44 -0800

    update solc

--- contracts/bleh.py
+++ contracts/bleh.py
@@ -1,11 +1,40 @@
-yo = 21000000000000000000000000
-i = yo * 0.03
-counter = 0
-all = 0
+# from math import floor
 
-while i >= 1:
-    print(i, yo, counter, counter / 365, all)
-    all += i
-    counter += 1
-    yo = yo - i
-    i = yo * 0.03
+# rewardsPerSecond = 1e18
+# totalCoinsRewarded = 0
+# for i in range(7304000):
+#     totalCoinsRewarded += rewardsPerSecond
+#     print(rewardsPerSecond, totalCoinsRewarded / 1e18, i)
+#     rewardsPerSecond *= 0.999
+#     if rewardsPerSecond <= 1:
+#         break
+
+# print(rewardsPerSecond, floor(totalCoinsRewarded / 1e18))
+
+
+tokensLeft = 21000000e18
+reward = 21000000e18
+totalRewards = 0
+i = 0
+while reward > 99:
+    reward = tokensLeft / 100
+    totalRewards += reward
+    tokensLeft -= reward
+    i += 1
+    print(reward, totalRewards, tokensLeft, i)
+
+
+# import random
+# # from math import round
+
+# whimsicalCharacters = ["Rupert (Mime)", "Sandy (Ventriloquist)", "Roberta (Fairy)", "Sidney (Barbie Girl Living in a Barbie World)"]
+
+# # print(round(random.random()))
+
+# print("4 jolly good friends get on 'the ride'... who will survive???")
+
+# for dude in whimsicalCharacters:
+#     if round(random.random()) == 0:
+#         print(dude, "sadly died on the rollercoaster known as life.")
+#     else:
+#         print(dude, "made it! But there's always tomorrow!")
